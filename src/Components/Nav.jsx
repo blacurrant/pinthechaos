a232b49318228a6c4c54d9470a4b07f1a64e5b66
blacurrant <122564843+blacurrant@users.noreply.github.com>
2023-03-07 20:14:49 +0530

    comitt

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,16 +3,16 @@ import { useState } from 'react';
 
 const SearchBar = ( { onSubmit } ) => {
 
-  const [term, setTerm] = useState('');
+  const [find, setFind] = useState('');
 
   const handleChange = (event) => {
-    setTerm(event.target.value);
+    setFind(event.target.value);
   };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
 
-    onSubmit(term)
+    onSubmit(find)
   };
 
   return (
@@ -21,7 +21,7 @@ const SearchBar = ( { onSubmit } ) => {
       <form onSubmit={onFormSubmit} className="flex items-center">   
           <label for="simple-search" className="sr-only">Search</label>
           <div className="relative w-full">
-              <input placeholder='search' value={term} onChange={handleChange} type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-20 pr-80 p-2.5 my-5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+              <input placeholder='search' value={find} onChange={handleChange} type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-20 pr-80 p-2.5 my-5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
       </form>
     </div>
